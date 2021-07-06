685ed02bbee5d91ba7d783af5166bfa8cbf90496
Daniel Chen <duhe.chen1009@gmail.com>
2021-07-06 14:14:02 -0700

    Clean up config files and unused code and update all dependencies

---
+++ core/src/main/java/net/danielchen/core/Asteroids.java
@@ -0,0 +1,57 @@
+package net.danielchen.core;
+
+import playn.core.Clock;
+import playn.core.Keyboard;
+import playn.core.Platform;
+import playn.core.Surface;
+import playn.scene.Layer;
+import playn.scene.SceneGame;
+import pythagoras.f.IDimension;
+
+public class Asteroids extends SceneGame {
+  private final Game game;
+
+  public Asteroids(Platform plat) {
+    super(plat, 33); // update our "simulation" 33ms (30 times per second)
+    // figure out how big the game view is
+    final IDimension size = plat.graphics().viewSize;
+    game = new Game();
+    plat.input().keyboardEvents.connect(new Keyboard.KeySlot() {
+      @Override
+      public void onEmit(Keyboard.KeyEvent event) {
+
+        switch (event.key) {
+          case LEFT:
+            game.setTurningLeft(event.down);
+            break;
+          case UP:
+            game.setAccelerating(event.down);
+            break;
+          case RIGHT:
+            game.setTurningRight(event.down);
+            break;
+          case SPACE:
+            game.setFiring(event.down);
+            break;
+          default:
+            break; // nada
+        }
+      }
+    });
+    // create a layer that just draws a black background
+    rootLayer.add(new Layer() {
+      protected void paintImpl(Surface surf) {
+        surf.setFillColor(0xFFFFFFFF).fillRect(0, 0, size.width(), size.height());
+      }
+    });
+
+    // create and add a board view
+    rootLayer.addCenterAt(new GameView(game, size), size.width() / 2, size.height() / 2);
+  }
+
+  @Override
+  public void update(Clock clock) {
+    super.update(clock);
+    game.update();
+  }
+}
---
+++ core/src/main/java/net/danielchen/core/Ship.java
@@ -0,0 +1,43 @@
+package net.danielchen.core;
+
+import org.jbox2d.dynamics.World;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Ship extends Entity {
+    private static final double SIZE = 0.025;
+    private static final double ANGLE = Math.PI / 3;
+    double acceleration;
+    boolean turningLeft, turningRight;
+
+    public Ship(World world, Point center) {
+        super("SHIP");
+        this.setPrimaryBody(new EntityBody(world, new ArrayList<>(Arrays.asList(
+                new Point(4 * SIZE / 5, 0),
+                new Point(SIZE * Math.sin(4 * ANGLE), SIZE * Math.cos(4 * ANGLE)),
+                new Point(SIZE * Math.sin(-ANGLE), SIZE * Math.cos(-ANGLE)))), center));
+    }
+
+    @Override
+    public void update() {
+        if (turningLeft) rotateTravel(-Math.PI / 12);
+        if (turningRight) rotateTravel(Math.PI / 12);
+        double ax = acceleration * Math.cos(travelAngle) - dx * Math.abs(dx) * 1;
+        double ay = acceleration * Math.sin(travelAngle) - dy * Math.abs(dy) * 1;
+        dx += ax;
+        dy += ay;
+        super.update();
+    }
+
+    @Override
+    public void rotateTravel(double angle) {
+        super.rotateTravel(angle);
+        super.rotateBody(angle);
+    }
+
+    public void accelerate(double a) {
+        this.acceleration = a;
+    }
+
+}
